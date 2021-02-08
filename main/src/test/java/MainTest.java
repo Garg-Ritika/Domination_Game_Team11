9b48a2a69edb04000920b6bbca99c8d6d87a8f66
Gurseerut Kaur <gurseerutkaur@gmail.com>
2021-02-08 13:12:19 +0530

    adding initial test case

---
+++ main/src/test/java/MainTest.java
@@ -0,0 +1,23 @@
+import junit.framework.TestCase;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+
+public class MainTest extends TestCase {
+
+    public MainTest(String testname){
+        super( testname);
+    }
+
+    public static Test suite(){
+        return  new TestSuite( MainTest.class);
+    }
+
+    public void testApp(){
+        assertTrue(true);
+    }
+    public void testApp2(){
+        assertTrue(false);
+    }
+
+}
