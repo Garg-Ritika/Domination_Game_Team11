5ec000fe7599a8e73457260dc57d9a9446fc5ed9
Gurseerut Kaur <gurseerutkaur@gmail.com>
2021-02-11 13:35:16 +0530

    initial test class for Country

--- main/src/test/java/ca/concordia/model/CountryTest.java
+++ main/src/test/java/ca/concordia/model/CountryTest.java
@@ -1,5 +1,29 @@
 package ca.concordia.model;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CountryTest {
+
+
+    @Test
+    public void getName() {
+        Country co=new Country(6,2,"Quebec-North",430,261);
+        assertEquals("Quebec-North",co.getName());
+    }
+
+    @Test
+    public void getCountryID() {
+        Country co=new Country(6,2,"Quebec-North",430,261);
+        assertEquals(6,co.getCountryID());
+    }
+
+    @Test
+    public void getContinentID() {
+        Country co=new Country(6,2,"Quebec-North",430,261);
+        assertEquals(2,co.getContinentID());
+    }
+
 
 }
