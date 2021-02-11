e59b8663acd95e38e0734b0e887b8a7f577a76ed
Gurseerut Kaur <gurseerutkaur@gmail.com>
2021-02-11 12:01:04 +0530

    initial test class for continent

--- main/src/test/java/ca/concordia/model/ContinentTest.java
+++ main/src/test/java/ca/concordia/model/ContinentTest.java
@@ -1,4 +1,33 @@
 package ca.concordia.model;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ContinentTest {
+
+
+    @Test
+    public void getID() {
+        Continent c = new Continent(2, "Ontario_and_Quebec", 4, "cyan");
+        assertEquals(2, c.getID());
+    }
+
+    @Test
+    public void getName() {
+        Continent c = new Continent(2, "Ontario_and_Quebec", 4, "cyan");
+        assertEquals("Ontario_and_Quebec", c.getName());
+    }
+
+    @Test
+    public void getArmyCount() {
+        Continent c = new Continent(2, "Ontario_and_Quebec", 4, "cyan");
+        assertEquals(4, c.getArmyCount());
+    }
+
+    @Test
+    public void getColor() {
+        Continent c = new Continent(2, "Ontario_and_Quebec", 4, "cyan");
+        assertEquals("cyan", c.getColor());
+    }
 }
