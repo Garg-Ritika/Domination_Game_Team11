1905b25070761dbe787a5ec644e186d09193c7f6
garg_ritikaa <sai computer@saicomputer>
2021-02-15 19:32:30 +0530

    Added Player model class structure

--- main/src/main/java/ca/concordia/model/Order.java
+++ main/src/main/java/ca/concordia/model/Order.java
@@ -4,8 +4,23 @@ public class Order {
 
     private int orderID;
 
+    public Order(int orderID) {
+        this.orderID = orderID;
+    }
+
+    /**
+     * Getters and Setters
+     **/
+    public int getOrderID() {
+        return orderID;
+    }
+
+    public void setOrderID(int orderID) {
+        this.orderID = orderID;
+    }
+
     public void execute() {
-        // to enact the order
+        // to enact the order- more clarity on this required?
     }
 
 }
