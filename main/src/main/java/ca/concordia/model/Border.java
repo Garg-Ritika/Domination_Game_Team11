ba02da032df02cfc0017284580681dea2dd58e2b
garg_ritikaa <gargritika2596@gmail.com>
2021-02-17 03:30:13 +0530

    added javadocs for Border and Country class

--- main/src/main/java/ca/concordia/model/Border.java
+++ main/src/main/java/ca/concordia/model/Border.java
@@ -5,41 +5,86 @@ package ca.concordia.model;
 
 import java.util.List;
 
+/**
+ * This class contains details of the border
+ *
+ * @author to be updated
+ *
+ */
 public class Border {
 
-    private int countryId;
-    private List<Integer> neighbours;
+    private int d_CountryId;
+    private List<Integer> d_Neighbours;
 
-    public Border(int countryId, List<Integer> neighbours) {
-        this.countryId = countryId;
-        this.neighbours = neighbours;
+    /**
+     * Constructor initializes list of neighbors and the countryId
+     *
+     * @param p_CountryId initializing the CountryID
+     * @param p_Neighbours initializing the neighbour list with corresponding CountryId
+     */
+    public Border(int p_CountryId, List<Integer> p_Neighbours) {
+        this.d_CountryId = p_CountryId;
+        this.d_Neighbours = p_Neighbours;
     }
 
+    /**
+     * This method returns the CountryId
+     *
+     * @return CountryId
+     */
     public int getCountryId() {
-        return countryId;
+
+        return d_CountryId;
     }
 
+    /**
+     * This method returns the Neighbours list associated with a country
+     *
+     * @return list of Neighbours
+     */
     public List<Integer> getNeighbours() {
-        return neighbours;
+        return d_Neighbours;
     }
 
-    public void setCountryId(int countryId) {
-        this.countryId = countryId;
+    /**
+     * This method sets the ID of the country.
+     *
+     * @param p_CountryId CountryId
+     */
+    public void setCountryId(int p_CountryId) {
+        this.d_CountryId = p_CountryId;
     }
 
-    public void setNeighbours(List<Integer> neighbours) {
-        this.neighbours = neighbours;
+    /**
+     * This method sets the Neighbours list of the country
+     *
+     * @param p_Neighbours list of Neighbours of the country
+     */
+    public void setNeighbours(List<Integer> p_Neighbours) {
+        this.d_Neighbours = p_Neighbours;
     }
 
-    public boolean addNeighbour(int neighbourCountryId) {
-        getNeighbours().add(neighbourCountryId);
+    /**
+     * This method adds the NeighbourCountryID to the Neighbours list of the country
+     *
+     * @param p_NeighbourCountryId Neighbours Country ID
+     * @return true if neighbourCountryId is succesfully added to lit of Neighbours
+     */
+    public boolean addNeighbour(int p_NeighbourCountryId) {
+        getNeighbours().add(p_NeighbourCountryId);
         return true;
     }
 
-    public boolean removeNeighbour(int neighbourCountryID) {
-        for (int val : getNeighbours()) {
-            if (val == neighbourCountryID) {
-                getNeighbours().remove(val);
+    /**
+     * This method removes the NeighbourCountryId value from the Neighbours list of the country if exists
+     *
+     * @param p_NeighbourCountryId Neighbours Country ID associated with a country to be removed
+     * @return true if the neighbourCountryId is successfully deleted i.e it exists else return false
+     */
+    public boolean removeNeighbour(int p_NeighbourCountryId) {
+        for (int l_Val : getNeighbours()) {
+            if (l_Val == p_NeighbourCountryId) {
+                getNeighbours().remove(l_Val);
                 return true;
             }
         }
