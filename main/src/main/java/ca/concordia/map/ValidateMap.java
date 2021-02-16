c4e772ccabc302f05fe9eaff66faa537a150bb47
Satinder Pal Singh <satinderpal.singh097@gmail.com>
2021-02-16 12:11:51 +0530

    updated the validatemap and graph file

--- main/src/main/java/ca/concordia/map/ValidateMap.java
+++ main/src/main/java/ca/concordia/map/ValidateMap.java
@@ -1,6 +1,10 @@
 package ca.concordia.map;
 
+import ca.concordia.model.Border;
+import ca.concordia.model.Graph;
 import ca.concordia.model.Map;
+
+import java.util.Stack;
 
 public class ValidateMap {
 
@@ -12,7 +16,125 @@ public class ValidateMap {
 
     public boolean validate() {
 
-        // TODO: add changes in this file..
+        if (map.getListOfContinents().size() < 0 || map.getListOfCountries().size() < 0) {
+            System.out.println(" No countries or continents available ");
+            return false;
+        }
+
+        if (!noDuplicates()) {
+            return false;
+        }
+
+        if (!DFS(getAdjacencyMatrix())) {
+            return false;
+        }
+
+        return true;
+
+    }
+
+    /**
+     * method to check if duplicate countries or continents exists ..
+     */
+    private boolean noDuplicates() {
+
+        for (int i = 0; i < map.getListOfContinents().size() - 1; i++) {
+            for (int j = i + 1; j < map.getListOfContinents().size(); j++) {
+
+                if (map.getListOfContinents().get(i).getID() == map.getListOfContinents().get(j).getID()) {
+                    System.out.println("duplicate continent found: " + map.getListOfContinents().get(i).getID());
+                    return false;
+                }
+            }
+        }
+
+        for (int i = 0; i < map.getListOfCountries().size() - 1; i++) {
+            for (int j = i + 1; j < map.getListOfCountries().size(); j++) {
+
+                if (map.getListOfCountries().get(i).getCountryID() == map.getListOfCountries().get(j).getCountryID()) {
+                    System.out.println("duplicate country found : " + map.getListOfCountries().get(i).getCountryID());
+                    return false;
+                }
+            }
+        }
         return true;
     }
+
+    /**
+     * Create adjacency matrix to represent graph using map
+     *
+     * @return graph
+     */
+    private Graph getAdjacencyMatrix() {
+        int numberOfCountries = map.getListOfCountries().size();
+        Graph graph = new Graph(numberOfCountries);
+        for (int i = 0; i < map.getListOfBorders().size(); i++) {
+            Border border = map.getListOfBorders().get(i);
+            for (int countryId : border.getNeighbours()) {
+                graph.addEdge(border.getCountryId(), countryId);
+            }
+
+        }
+        return graph;
+    }
+
+    /**
+     * Use depth-first-search in directed connected graph  to find whether it is connected or not.
+     *
+     * @param graph
+     * @return
+     */
+    private boolean DFS(Graph graph) {
+        if (graph.size() < 1) {
+            return false;
+        }
+
+        Stack<Integer> stack = new Stack<Integer>();
+        int source = 1; // assume the source starts from country ID 1
+        int nodeCount = graph.getAdjMatrix()[source].length - 1; // another way of finding number of countries, starting from 0
+        boolean[][] matrix = graph.getAdjMatrix();
+        boolean visited[] = new boolean[nodeCount + 1];
+        int element = source;
+        int i = source;
+
+        // use stack to create a visited list
+        visited[source] = true;
+        stack.push(source);
+        while (!stack.isEmpty()) {
+            element = stack.peek();
+            i = element;
+
+            while (i <= nodeCount) {
+
+                if (matrix[element][i] == true && visited[i] == false) {
+                    stack.push(i);
+                    visited[i] = true;
+                    element = i;
+                    i = 1;
+                    continue;
+                }
+                i++;
+            }
+            stack.pop();
+        }
+
+        // find connectivity from visited list
+        boolean connected = false;
+        for (int node = 1; node <= nodeCount; node++) {
+            if (visited[node] == true) {
+                connected = true;
+            } else {
+                connected = false;
+                break;
+            }
+        }
+
+        if (connected) {
+            System.out.println("it is a connected graph");
+        } else {
+            System.out.println("It is NOT a connected graph");
+        }
+        return connected;
+    }
+
 }
---
+++ main/src/main/java/ca/concordia/model/Graph.java
@@ -0,0 +1,53 @@
+package ca.concordia.model;
+
+/**
+ * Adjacency Matrix representation of the Maps
+ *
+ * @author
+ */
+
+public class Graph {
+
+    private boolean adjMatrix[][];
+    private int numberOfCountries; // represent number of vertices
+
+    public Graph(int numberOfCountries) {
+        this.numberOfCountries = numberOfCountries;
+        this.adjMatrix = new boolean[numberOfCountries][numberOfCountries];
+    }
+
+    public void addEdge(int i, int j) {
+        adjMatrix[i][j] = true;
+        adjMatrix[j][i] = true;
+    }
+
+    public void removeEdge(int i, int j) {
+        adjMatrix[i][j] = false;
+        adjMatrix[j][i] = false;
+    }
+
+    public boolean[][] getAdjMatrix() {
+        return adjMatrix;
+    }
+
+    public int size() {
+        return this.numberOfCountries * this.numberOfCountries;
+    }
+
+    /**
+     * Create string format of Adjacency matrix
+     *
+     * @return matrix string
+     */
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < numberOfCountries; i++) {
+            sb.append(i + ": ");
+            for (boolean j : adjMatrix[i]) {
+                sb.append((j ? 1 : 0) + " ");
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+}
