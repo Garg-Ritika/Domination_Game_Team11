adc8997560cdffcae41bfe7360e05f8d49b416df
Nilesh Aggarwal <agnilesh8199@gmail.com>
2021-02-16 16:02:46 +0530

    Added game package and files GameEngine and PlayerActions

---
+++ main/src/main/java/ca/concordia/game/GameEngine.java
@@ -0,0 +1,72 @@
+package ca.concordia.game;
+
+import ca.concordia.model.Map;
+import ca.concordia.model.Player;
+
+public class GameEngine {
+
+    private static GameEngine instance = null;
+    private final Map currentMap;
+    private final PlayerActions playerActions;
+
+    public static GameEngine getInstance(Map map) {
+        if (instance == null) {
+            instance = new GameEngine(map);
+        }
+        return instance;
+    }
+
+    private GameEngine(Map map) {
+        this.currentMap = map;
+        this.playerActions = new PlayerActions(this.currentMap);
+        this.loadMapforGame(); // automatically load game in the constructor
+    }
+
+    private void loadMapforGame() {
+        mainGameLoop();
+    }
+
+    public boolean addPlayer(Player player){
+        return this.playerActions.addPlayer(player);
+    }
+
+    public boolean removePlayer(Player player){
+        return this.playerActions.removePlayer(player);
+    }
+
+    /**
+     *  Main Game loop that does three things for each player:
+     *  1. Assign reinforcements
+     *  2. Issue Orders
+     *  3. Execute Orders
+     *
+     *  (After above three steps, it self-exit the game.)
+     */
+
+    private void mainGameLoop(){
+        this.playerActions.assignCountriesToPlayers();
+
+        boolean gameOver = false;
+        while (true){
+            for(Player player: this.playerActions.getListOfPlayers()){
+                System.out.println("----------------------------------------------------------------");
+                System.out.println("ASSIGN REINFORCEMENT PHASE : " + player.getPlayerName());
+                this.playerActions.assignReinforcementPhase(player);
+
+                System.out.println("----------------------------------------------------------------");
+                System.out.println("ISSUE ORDER PHASE : " + player.getPlayerName());
+                this.playerActions.issueOrdersPhase(player);
+
+                System.out.println("----------------------------------------------------------------");
+                System.out.println("EXECUTE ORDER  PHASE : " + player.getPlayerName());
+                this.playerActions.executeOrderPhase(player);
+
+            }
+            if(this.playerActions.isGameOver()){
+                System.out.println("GAME OVER");
+                break;
+            }
+        }
+        System.out.println("Winner is player > id: " + playerActions.getWinner().getPlayerID() + " name: " + playerActions.getWinner().getPlayerName());
+    }
+}
---
+++ main/src/main/java/ca/concordia/game/PlayerActions.java
@@ -0,0 +1,146 @@
+package ca.concordia.game;
+
+import ca.concordia.model.Country;
+import ca.concordia.model.Map;
+import ca.concordia.model.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ *  Helper class that is used to
+ *  1. add player into player list
+ *  2. remove player from player list
+ *  3. assign countries to the player
+ *  4. assign armies to their countries of the players *
+ *
+ *   @author: Nilesh
+ */
+
+//TODO : do we want to make this a singleton class ?
+public class PlayerActions {
+
+    private final int MINIMUM_PLAYER_COUNT = 3;
+    private List<Player> listOfPlayers = new ArrayList<Player>();
+    private final Map map;
+    private Player winner;
+
+    public PlayerActions(Map map) {
+        this.map = map;
+        this.winner = null;
+    }
+
+    public boolean addPlayer(Player player) {
+        //TODO: cannot add more player than number of countries check
+        return this.listOfPlayers.add(player);
+    }
+
+    public boolean removePlayer(Player player) {
+        return this.listOfPlayers.remove(player);
+    }
+
+    public List<Player> getListOfPlayers(){
+        return this.listOfPlayers;
+    }
+
+    /**
+     * This method assigns countries to players
+     */
+    public void assignCountriesToPlayers() {
+        if (listOfPlayers.size() < MINIMUM_PLAYER_COUNT) {
+            System.out.println("Number of players should be atleast " + MINIMUM_PLAYER_COUNT + " to start assigning countries");
+            return;
+        }
+
+        int countryCount = this.map.getListOfCountries().size();
+        int playerCount = this.listOfPlayers.size();
+        int playerCountryRatio = countryCount/playerCount;
+
+        // temporary country list that will be randomly assigned in quantity playCountryRatio to each player ..
+        ArrayList<Country> countriesToAssignRandomly = new ArrayList<Country>();
+        for (Country country: map.getListOfCountries()){
+            countriesToAssignRandomly.add(country);
+        }
+
+        for (Player player: listOfPlayers){
+
+            ArrayList<Country> countriesForPlayer = new ArrayList<Country>();
+            while((countriesForPlayer.size() < playerCountryRatio)) {
+
+                int index = 0;
+                if(countriesToAssignRandomly.size() <1){
+                    System.out.println("All countries are assigned successfully ");
+                    break;
+                }else if(countriesToAssignRandomly.size() > 1){
+                    index = new Random().nextInt(countriesToAssignRandomly.size() -1);
+                }
+                Country country = countriesToAssignRandomly.get(index);
+                countriesToAssignRandomly.remove(country);
+                countriesForPlayer.add(country);
+            }
+            player.setListOfCountries(countriesForPlayer);
+        }
+    }
+
+    /**
+     * This method assigns armies to the players and their countries..
+     */
+    public void assignReinforcementPhase(Player player) {
+        // TODO: what is the logic here ?
+
+    }
+
+    /**
+     * The GameEngine class calls the issue_order() method of the Player.
+     * This method will wait for the following command, then create a deploy order object on the player’s list of orders,
+     * then reduce the number of armies in the player’s reinforcement pool.
+     *
+     * The game engine does this for all players in round-robin fashion until all the players have placed all their reinforcement armies on the map.
+     *
+     * Issuing order command:
+     * deploy countryID num (until all reinforcements have been placed)
+     *
+     */
+    public void issueOrdersPhase(Player player){
+
+        // round robin means, every player should interact with other player .. ?
+        for (Player nextPlayer: this.listOfPlayers){
+
+            if (nextPlayer.getPlayerID() == player.getPlayerID()){
+                continue;
+            }
+            //TODO: how "deploy countryID num* command will be used here ?
+            player.issueOrder();
+        }
+    }
+
+
+    /**
+     *
+     * @param player
+     * @return
+     */
+    public void executeOrderPhase(Player player){
+        player.nextOrder();
+        // TODO: how to execute order ?
+        // TODO: what are thing required here ?
+    }
+
+    /**
+     * Game is only over when one player has captured all the countries ..
+     * @return
+     */
+    public boolean isGameOver(){
+        // TODO :
+        return true;
+    }
+
+    public void setWinner(Player player){
+        this.winner= player;
+    }
+
+    public Player getWinner(){
+        return this.winner;
+    }
+}
