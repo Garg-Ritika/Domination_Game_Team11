1f5a842760b037cf5e4c2522f640b27b35363123
Gurseerut Kaur <gurseerutkaur@gmail.com>
2021-02-22 10:03:00 +0530

    updating BorderTest class

--- main/src/test/java/ca/concordia/model/BorderTest.java
+++ main/src/test/java/ca/concordia/model/BorderTest.java
@@ -9,32 +9,48 @@ import java.util.List;
 import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertEquals;
 
+/**
+ * This class contains test details of Border class
+ *
+ * @author to be updated
+ */
 public class BorderTest {
-    private List<Integer> neighbours= new LinkedList<>(Arrays.asList(1,2,3,4));
+    private List<Integer> d_Neighbours= new LinkedList<>(Arrays.asList(1,2,3,4));
 
-
+    /**
+     * This method uses assert statement to test expected CountryId with actual CountryId
+     */
     @Test
     public void getCountryId() {
-        Border b= new Border(4, neighbours  );
+        Border b= new Border(4, d_Neighbours  );
         assertEquals(4, b.getCountryId());
     }
 
+    /**
+     * This method tests whether expected neighbors matches with the actual neighbors
+     */
     @Test
     public void getNeighbours() {
-        Border b= new Border(4, neighbours  );
-        assertEquals(neighbours, b.getNeighbours());
+        Border b= new Border(4, d_Neighbours  );
+        assertEquals(d_Neighbours, b.getNeighbours());
     }
 
+    /**
+     * This method tests the method of addNeighbors in Border clas
+     */
     @Test
     public void addNeighbour() {
-        Border b= new Border(4, neighbours  );
+        Border b= new Border(4, d_Neighbours  );
         assertTrue(b.addNeighbour(7));
 
     }
 
+    /**
+     * This method tests whether removeNeighbor method in border class is correct or not
+     */
     @Test
     public void removeNeighbour() {
-        Border b= new Border(4, neighbours  );
+        Border b= new Border(4, d_Neighbours  );
         assertTrue(b.removeNeighbour(3));
 
     }
