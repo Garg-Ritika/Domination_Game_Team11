35d2ce963be990960226819eec005cf46219973c
Satinder Pal Singh <satinderpal.singh097@gmail.com>
2021-02-23 18:00:08 +0530

    Observable class updated

--- main/src/main/java/ca/concordia/Observable.java
+++ main/src/main/java/ca/concordia/Observable.java
@@ -13,27 +13,27 @@ public class Observable {
     private List<Observer> d_Observers = new ArrayList<Observer>();
     /**
      * attach a view to the model.
-     * @param p_o: view to be added to the list of IObservers to be notified.
+     * @param p_O: view to be added to the list of IObservers to be notified.
      */
-    public void attach(Observer p_o) {
-        this.d_Observers.add(p_o);
+    public void attach(Observer p_O) {
+        this.d_Observers.add(p_O);
     }
     /**
      * detach a view from the model.
-     * @param p_o: view to be removed from the list of IObservers.
+     * @param p_O: view to be removed from the list of IObservers.
      */
-    public void detach(Observer p_o) {
+    public void detach(Observer p_O) {
         if (!d_Observers.isEmpty()) {
-            d_Observers.remove(p_o);
+            d_Observers.remove(p_O);
         }
     }
     /**
      * Notify all the views attached to the model.
-     * @param p_o: object that contains the information to be observed.
+     * @param p_O: object that contains the information to be observed.
      */
-    public void notifyObservers(Observable p_o) {
+    public void notifyObservers(Observable p_O) {
         for (Observer Observer : d_Observers) {
-            Observer.update(p_o);
+            Observer.update(p_O);
         }
     }
 }
