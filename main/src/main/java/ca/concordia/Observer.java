d29a40e1e72a92c6d319578179b143e3f708f692
Satinder Pal Singh <satinderpal.singh097@gmail.com>
2021-02-23 18:05:24 +0530

    Observer class coding convention

--- main/src/main/java/ca/concordia/Observer.java
+++ main/src/main/java/ca/concordia/Observer.java
@@ -10,8 +10,8 @@ public interface Observer {
      * method to be implemented that reacts to the notification generally by
      * interrogating the model object and displaying its newly updated state.
      *
-     * @param p_observable_state: Object that is passed by the subject (observable). Very often, this
+     * @param p_ObservableState: Object that is passed by the subject (observable). Very often, this
      *           object is the subject itself, but not necessarily.
      */
-    public void update(Observable p_observable_state);
+    public void update(Observable p_ObservableState);
 }
