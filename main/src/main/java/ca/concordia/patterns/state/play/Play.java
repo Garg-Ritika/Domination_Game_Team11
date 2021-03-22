81dcd656b27308d7fe974b47a33e10b8c12a114e
Satinder Pal Singh <satinderpal.singh097@gmail.com>
2021-03-22 17:35:18 +0530

    Updated javadoc for Play class

--- main/src/main/java/ca/concordia/patterns/state/play/Play.java
+++ main/src/main/java/ca/concordia/patterns/state/play/Play.java
@@ -8,21 +8,35 @@ import ca.concordia.gameengine.GameEngine;
 import ca.concordia.patterns.state.Phase;
 import ca.concordia.patterns.state.end.End;
 
+/**
+ * This class extends phase class
+ * It executes showMap command
+ * and for loadMap, editContinent, editCountry, editNeighbour,saveMap, editMap, validateMap, setPlayers, assignCountries,
+ * reinforce, attack, fortify it shows invalid command message
+ */
 public class Play extends Phase {
 
     final int MINIMUM_PLAYER_COUNT = 3;
 
+    /**
+     * Constructor that contains GameEngine object as argument
+     * @param p_ge GameEngine object
+     */
     public Play(GameEngine p_ge) {
         super(p_ge);
     }
 
+    /**
+     * loadMap method takes command as input and shows invalid commadn message
+     * @param p_Command loadMap command
+     */
     @Override
     public void loadMap(String[] p_Command) {
         printInvalidCommandMessage();
     }
 
     /**
-     * triggers on "showmap" command, this is a different function than MapEditors' showmap commad
+     * triggers on "showMap" command, this is a different function than MapEditors' showMap command
      * as it shows extra details like :
      * 1. show all countries
      * 2. show all continents
@@ -54,67 +68,114 @@ public class Play extends Phase {
         }
     }
 
+    /**
+     * editContinent method takes command as input and shows invalid command message to user
+     * @param p_Command editContinent command
+     */
     @Override
     public void editContinent(String[] p_Command) {
         printInvalidCommandMessage();
     }
 
+    /**
+     * editCoountry method takes command as input and shows invalid command message to user
+     * @param p_Command editCoountry command
+     */
     @Override
     public void editCountry(String[] p_Command) {
         printInvalidCommandMessage();
     }
 
+    /**
+     * editNeighbour method takes command as input and shows invalid command message to user
+     * @param p_Command editNeighbour command
+     */
     @Override
     public void editNeighbour(String[] p_Command) {
         printInvalidCommandMessage();
     }
+    /**
+     * This method takes saveMap command as input and shows invalid command message to user
+     * @param p_Command saveMap command
+     */
 
     @Override
     public void saveMap(String[] p_Command) {
         printInvalidCommandMessage();
     }
 
+    /**
+     * This method takes command as input and shows invalid command message to user
+     * @param p_Command editMap command
+     */
     @Override
     public void editMap(String[] p_Command) {
         printInvalidCommandMessage();
     }
 
+    /**
+     * This method takes validateMap command as input and shows invalid command message to user
+     * @param p_Command evalidateMap command
+     */
     @Override
     public void validateMap(String[] p_Command) {
         printInvalidCommandMessage();
     }
 
+    /**
+     * setPlayers method takes command as input and shows invalid command message to user
+     * @param p_Command setPlayers command
+     */
     @Override
     public void setPlayers(String[] p_Command) {
         printInvalidCommandMessage();
     }
 
+
+    /**
+     * assignCountries method prints invalid command message to the user
+     */
     @Override
     public void assignCountries() {
         printInvalidCommandMessage();
     }
 
+    /**
+     * reinforce method prints invalid command message to the user
+     */
     @Override
     public void reinforce() {
         printInvalidCommandMessage();
     }
 
+    /**
+     * this method shows invalid command message to the user
+     */
     @Override
     public void attack() {
         printInvalidCommandMessage();
     }
 
+    /**
+     * fortify method shows invalid command message to the user
+     */
     @Override
     public void fortify() {
         printInvalidCommandMessage();
     }
 
+    /**
+     * This method changes the phase to endGame
+     */
     @Override
     public void endGame() {
         d_ge.setPhase(new End(d_ge));
         d_ge.getPhase().endGame();
     }
 
+    /**
+     * This method moves to next phase
+     */
     @Override
     public void next() {
 
