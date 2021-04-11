e7b483ca526e292d9e3fb5185a128327b00adbad
Satinder Pal Singh <satinderpal.singh097@gmail.com>
2021-04-11 23:10:25 +0530

    javadoc of Aggressive class done

--- main/src/main/java/ca/concordia/patterns/strategy/Aggressive.java
+++ main/src/main/java/ca/concordia/patterns/strategy/Aggressive.java
@@ -11,16 +11,30 @@ import java.util.LinkedList;
  * An aggressive computer player strategy that focuses on centralization of forces and then attack, i.e. it deploys on its strongest
  * country, then always attack with its strongest country, then moves its armies in order to maximize aggregation of forces in one
  * country.
+ * This class extends abstract strategy class
  */
 
 public class Aggressive extends Strategy {
 
+    /**
+     * Initialized linked list of orders
+     */
     private LinkedList<Order> d_ListOfOrders = new LinkedList<Order>();
 
+    /**
+     * Constructor that includes GameEngine object as its parameters
+     * @param p_Ge GameEngine object
+     */
     public Aggressive(GameEngine p_Ge) {
         super(p_Ge);
     }
 
+    /**
+     * Create method that takes player object as its parameters
+     * and return list of order
+     * @param p_Player
+     * @return
+     */
     @Override
     public LinkedList<Order> create(Player p_Player) {
         return d_ListOfOrders;
