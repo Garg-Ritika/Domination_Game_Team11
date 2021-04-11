d1c0e3ea7a71e560f91021b30957dc8c7d27a394
Gurseerut Kaur <gurseerutkaur@gmail.com>
2021-04-11 17:13:44 +0530

    Adding GameStatsTest class

---
+++ main/src/test/java/ca/concordia/gameengine/GameStatsTest.java
@@ -0,0 +1,31 @@
+package ca.concordia.gameengine;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class GameStatsTest {
+    GameStats gs;
+
+    @Before
+    public void before(){
+        gs= new GameStats("abc","12","def");
+    }
+
+    @Test
+    public void getMapName() {
+        assertEquals(true, gs.getMapName().contains("abc"));
+    }
+
+    @Test
+    public void getGameNumber() {
+        assertEquals(true, gs.getGameNumber().contains("12"));
+    }
+
+    @Test
+    public void getResult() {
+        assertEquals(true, gs.getResult().contains("def"));
+    }
+
+}
