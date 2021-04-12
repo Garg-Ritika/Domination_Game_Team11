24f06780956ce5939d495162db1dd393a6a5bb51
Satinder Pal Singh <satinderpal.singh097@gmail.com>
2021-04-12 13:41:32 +0530

    Update : GameSaver javadoc added

--- main/src/main/java/ca/concordia/gameengine/GameSaver.java
+++ main/src/main/java/ca/concordia/gameengine/GameSaver.java
@@ -5,6 +5,9 @@ import ca.concordia.patterns.observer.LogUtil;
 
 import java.io.*;
 
+/**
+ * GameSaver class saves the GameFile along MapLocation ,Phases & Players details.
+ */
 
 public class GameSaver {
 
@@ -13,6 +16,10 @@ public class GameSaver {
         d_Ge = p_Ge;
     }
 
+    /**
+     * This method is used for saving GameFile
+     * @param p_GameFile
+     */
     public void saveFile(File p_GameFile){
         try{
             FileWriter l_Fw = new FileWriter(p_GameFile, true);
@@ -31,15 +38,28 @@ public class GameSaver {
         }
     }
 
+    /**
+     * This method adds MapLocation to the GameFile
+     * @param p_Content
+     */
     private void addMapLocation(String p_Content){
         p_Content += ("\r\n[Map]\r\n");
         p_Content += (d_Ge.getMap().getName()+"\r\n");
     }
 
+    /**
+     * This method is used for getting Phase details
+     * @param p_Content
+     */
     private void addPhaseDetails(String p_Content){
         p_Content += ("\r\n[CurrentPhase]\r\n");
         p_Content += (d_Ge.getPhase().getClass().getName() +"\r\n");
     }
+
+    /**
+     * This method is used for getting Player Details 
+     * @param p_Content
+     */
 
     private void addPlayers(String p_Content){
         p_Content += ("\r\n[Players]\r\n");
